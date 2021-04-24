f59d378fef1f109c7be8375728803787377c5f67
Kid Vader <nickjamil@gmail.com>
2021-04-24 00:14:15 -0400

    Add filter using blocks and select

--- filter.rb
+++ filter.rb
@@ -1,13 +1,4 @@
 require "csv"
 
-def classic_jazz?(record)
-    record[2] == "Jazz" and record[3].to_i.between?(1950, 1959)
-end
-
 albums = CSV.read("albums.csv")[1..]
-
-for i in 0...albums.size do
-    if classic_jazz?(albums[i])
-        puts albums[i][0]
-    end
-end
+puts albums.select {|record| record[2] == "Jazz" and record[3].to_i.between?(1950, 1959)}
